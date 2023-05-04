745ed272df028269c99056d93d737e072d1f7265
Sidney <53360670+sidrodev@users.noreply.github.com>
2023-05-03 20:02:52 -0400

    updated packages and .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     browser: true,
     node: true,
     es6: true,
+  },
+  parserOptions: {
+    babelOptions: {
+      configFile: path.resolve(__dirname, '.babelrc'),
+    },
   },
 };
